49e146ba2513bd89c870cc25595856a7c22ac7b6
Konrad Kosmatka <kkonrad92@gmail.com>
2013-11-11 02:47:56 +0100

    added button for signal graph reset

--- alignment.c
+++ alignment.c
@@ -3,23 +3,40 @@
 #include "gui.h"
 #include "connection.h"
 #include "alignment.h"
+#include "graph.h"
 
 void alignment_dialog()
 {
-    GtkWidget *dialog = gtk_dialog_new_with_buttons("Antenna input alignment", GTK_WINDOW(gui.window), GTK_DIALOG_DESTROY_WITH_PARENT, GTK_STOCK_CLOSE, GTK_RESPONSE_ACCEPT, NULL);
+    GtkWidget *dialog = gtk_dialog_new_with_buttons("Antenna input alignment", GTK_WINDOW(gui.window), GTK_DIALOG_DESTROY_WITH_PARENT, NULL);
 
     GtkWidget *content = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
     gtk_container_set_border_width(GTK_CONTAINER(dialog), 5);
-    GtkWidget *d_box = gtk_hbox_new(FALSE, 5);
-    gtk_container_add(GTK_CONTAINER(content), d_box);
 
     GtkObject* adj = gtk_adjustment_new(daa, 0.0, 127.0, 1.0, 1.0, 0);
     GtkWidget *d_scale = gtk_hscale_new(GTK_ADJUSTMENT(adj));
     gtk_scale_set_digits(GTK_SCALE(d_scale), 0);
-    gtk_box_pack_start(GTK_BOX(d_box), d_scale, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(content), d_scale, TRUE, TRUE, 0);
     g_signal_connect(G_OBJECT(adj), "value-changed", G_CALLBACK(alignment_update), NULL);
     gtk_widget_set_size_request(d_scale, 384, -1);
-    gtk_scale_set_value_pos(GTK_SCALE(d_scale), GTK_POS_RIGHT);
+
+    GtkWidget *sep = gtk_hseparator_new();
+    gtk_box_pack_start(GTK_BOX(content), sep, TRUE, TRUE, 0);
+
+    GtkWidget *buttons_box = gtk_hbox_new(TRUE, 5);
+    gtk_container_add(GTK_CONTAINER(content), buttons_box);
+
+    GtkWidget *b_reset_rssi = gtk_button_new_with_label("Reset signal graph");
+    gtk_box_pack_start(GTK_BOX(buttons_box), b_reset_rssi, TRUE, TRUE, 0);
+    gtk_widget_set_can_focus(GTK_WIDGET(b_reset_rssi), FALSE);
+    g_signal_connect(b_reset_rssi, "clicked", G_CALLBACK(graph_clear), NULL);
+
+    GtkWidget *b_close = gtk_button_new_with_label("Close");
+    gtk_button_set_image(GTK_BUTTON(b_close), gtk_image_new_from_stock(GTK_STOCK_CLOSE, GTK_ICON_SIZE_BUTTON));
+    gtk_box_pack_start(GTK_BOX(buttons_box), b_close, TRUE, TRUE, 0);
+    gtk_widget_set_can_focus(GTK_WIDGET(b_close), FALSE);
+    g_signal_connect_swapped(b_close, "clicked", G_CALLBACK(gtk_widget_destroy), dialog);
+    g_signal_connect_swapped(dialog, "response", G_CALLBACK (gtk_widget_destroy), dialog);
+
     gtk_widget_show_all(dialog);
 #ifdef G_OS_WIN32
     // workaround for windows to keep dialog over main window
@@ -35,7 +52,6 @@ void alignment_dialog()
 #else
     gtk_dialog_run(GTK_DIALOG(dialog));
 #endif
-    gtk_widget_destroy(dialog);
 }
 
 void alignment_update(GtkAdjustment *get, gpointer nothing)
